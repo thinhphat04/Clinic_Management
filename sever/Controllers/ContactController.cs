4781308a41eef88bb357dc46522f5e4a2bfc6552
Tram <tranbaohuyentram96@gmail.com>
2024-01-31 12:39:30 +0700

    update contact

---
+++ sever/Controllers/ContactController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PJ_SEM03.Models;
+using PJ_SEM03.Repository;
+
+namespace PJ_SEM03.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactController : ControllerBase
+    {
+        private readonly IContactRepo repo;
+
+        public ContactController(IContactRepo repo)
+        {
+            this.repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Contact>>> GetAll()
+        {
+            try
+            {
+                return Ok(await repo.GetAll());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet("{type}")]
+        public async Task<ActionResult<IEnumerable<Contact>>> GetByType(string type)
+        {
+            try
+            {
+                return Ok(await repo.GetByType(type));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Contact>> CreateNew(Contact contact)
+        {
+            try
+            {
+                return Ok(await repo.CreateNew(contact));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
