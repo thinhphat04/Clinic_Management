e4fcfe470d0e446cde832a973eb64517356b6306
ngothinhphat <ngothinhphat244@gmail.com>
2024-02-02 01:10:07 +0700

    add productDto

---
+++ sever/DTO/productDTO.cs
@@ -0,0 +1,9 @@
+namespace PJ_SEM03.DTO;
+
+public class productDTO
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public IFormFile Image { get; set; }
+
+}
